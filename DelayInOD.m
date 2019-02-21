963e6efdd7b722ba409a90b6c451ea968726d532
AbrahamAvelar <jabrahamavelar@gmail.com>
2019-02-21 17:48:40 -0600

    incluye al último día y cambio de los ifs nargin <4 o <5

--- DelayInOD.m
+++ DelayInOD.m
@@ -2,24 +2,26 @@ function h = DelayInOD(bgdata, pl, pozo, dias, mapacolor)
 % DelayInOD(bgdata, pl, mapacolor)
 % 
     clf
-    NuevosDias=EncuentraDias(bgdata(pl),.3);
-    if nargin<5
-        mapacolor=jet;
-    end
+    NuevosDias=EncuentraDias(bgdata(pl),.2);
+    NuevosDias(end+1)= size(bgdata(pl).OD,1);
 	if nargin<4
         dias=1:length(NuevosDias)-1 ;
     end
-    
-    saltos=floor(length(mapacolor)/length(NuevosDias));
+	if nargin<5
+        mapacolor=hsv(length(dias));
+    end
+    %saltos=floor(length(mapacolor)/length(NuevosDias));
     %con=0;
    for i=dias
         %con=con+1;
         %subplot(length(dias),1,con)
         estedia=NuevosDias(i):NuevosDias(i+1)-1;
-        plot( (bgdata(pl).t(estedia)-bgdata(pl).t(estedia(1)))*24,  bgdata(pl).OD(estedia, pozo),'.-', 'color', mapacolor(i*saltos,:))
+        plot( (bgdata(pl).t(estedia)-bgdata(pl).t(estedia(1)))*24,  bgdata(pl).OD(estedia, pozo),'.-', 'color', mapacolor(i,:))
         hold on
-        xlim([0 22])
+        xlim([0 24])
+        tolegend{i}=num2str(i);
    end
     
+   legend(tolegend, 'location','best')
 
 end
