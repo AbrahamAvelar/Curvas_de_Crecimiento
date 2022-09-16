dced71a7e6542168f698abbe85bfbeaf408539d4
AbrahamAvelar <jabrahamavelar@gmail.com>
2022-09-15 23:09:20 -0500

    Update EncuentraExponencial.m

--- EncuentraExponencial.m
+++ EncuentraExponencial.m
@@ -1,9 +1,16 @@
-function PuntosExponencial = EncuentraExponencial(OD, t, plots)
+function PuntosExponencial = EncuentraExponencial(OD, t, plots, umbral)
+% Esta función toma en cuenta todos los pozos que le pasen. Si solo hay mediciones en 60 pozos, no se recomienda pasar los 96.
 % EncuentraExponencial(OD, t, plots)
 % si plots=1 hace dibujos
 % plots=0, no se detiene a hacer plots.
 % va haciendo robustfits de 3 en 3 puntos del t y la OD. 
-% cuando pasan el humbral find(pendientes>(max(pendientes)/2))
+% cuando pasan el umbral find(pendientes>(max(pendientes)/2))
+% umbral es un valor de 0 a 1 con la proporción de pozos en los que una medición específica del plato corresponde a la fase exponencial de >umbral pozos. Por ejemplo, si en la medición 2 del día 1 solo hay 5 pozos en fase exponencial y 91 no, entonces solo tomará esa medición si umbral<= 5/96
+
+if nargin < 4
+    umbral=0.5;
+end
+
 if plots
     figure(1)
     clf
@@ -30,8 +37,8 @@ end
 PuntosExponencialTemp(PuntosExponencialTemp==0) = NaN;
 [uv,~,idx] = unique(PuntosExponencialTemp);
 n = accumarray(idx(:),1);
-minimovalidas=floor(max(n)*.5);%0.9 es la fraccion de pozos que esperas que tengan curvas de crecimiento similares
-PuntosExponencial= uv(n>80)';
+minimovalidas=floor(max(n)*umbral);% es la fraccion de pozos que esperas que tengan curvas de crecimiento similares
+PuntosExponencial= uv(n>minimovalidas)'; # Aquí 
 
         if plots
             plot(t(PuntosExponencial), OD(PuntosExponencial,:),'ob')
